06688ca4a93676edae49052778c7feb2b6bc9ae5
Carter Page <cpage@pobox.com>
2012-03-13 00:41:49 -0400

    Fixed loader for band names

--- yoga/convertsongstosql.pl
+++ yoga/convertsongstosql.pl
@@ -11,21 +11,23 @@ while(<>) {
     $album = $a[1];
     $song = $a[2];
 
-    unless ($bandid{$band}) {
-	$bandid{$band} = ++$bandcount;
+    $bandkey = $band;
+    $bandkey =~ s/'//g;
+    unless ($bandid{$bandkey}) {
+	$bandid{$bandkey} = ++$bandcount;
 	$band =~ s/'/''/g;
 	print "INSERT INTO Artist(id, name)  VALUES($bandcount, '$band')\n";
     }
-    $albumkey = $bandid{$band} . "_" . $album;
+    $albumkey = $bandid{$bandkey} . "_" . $album;
     $albumkey =~ s/'//g;
     unless ($albumid{$albumkey}) {
 	$albumid{$albumkey} = ++$albumcount;
 	$album =~ s/'/''/g;
-	print "INSERT INTO Album(id, title, artistId, year)  VALUES($albumcount, '$album', $bandid{$band}, 1974)\n";
+	print "INSERT INTO Album(id, title, artistId, year)  VALUES($albumcount, '$album', $bandid{$bandkey}, 1974)\n";
     }
     ++$songcount;
     $song =~ s/'/''/g;
-    print "INSERT INTO Song(id, title, artistId, albumId)  VALUES($songcount, '$song', $bandid{$band}, "
+    print "INSERT INTO Song(id, title, artistId, albumId)  VALUES($songcount, '$song', $bandid{$bandkey}, "
 	. $albumid{$albumkey} . ")\n";
 
 }
